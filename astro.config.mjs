8c5db7c870f42003f834eb0ca0f9c09c69c8eaac
Omri Dagan <daganomri@gmail.com>
2023-07-27 20:24:11 +0300

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import netlify from "@astrojs/netlify/functions";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), mdx()],
+  output: "server",
+  adapter: netlify()
+});
